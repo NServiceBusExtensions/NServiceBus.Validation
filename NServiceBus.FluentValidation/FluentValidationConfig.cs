a7e61c397a6302dd1f835d1812c3807de4f87ccc
Simon Cropp <simon.cropp@gmail.com>
2018-10-24 11:25:49 +1100

    add throwForNoValidatorsFound

--- NServiceBus.FluentValidation/FluentValidationConfig.cs
+++ NServiceBus.FluentValidation/FluentValidationConfig.cs
@@ -43,18 +43,19 @@ namespace NServiceBus
             return new MessageValidator(validatorTypeCache);
         }
 
-        public void AddValidatorsFromAssemblyContaining<T>(bool throwForNonPublicValidators = true)
+        public void AddValidatorsFromAssemblyContaining<T>(bool throwForNonPublicValidators = true, bool throwForNoValidatorsFound = true)
         {
-            AddValidatorsFromAssemblyContaining(typeof(T), throwForNonPublicValidators);
+            AddValidatorsFromAssemblyContaining(typeof(T), throwForNonPublicValidators, throwForNoValidatorsFound);
         }
 
-        public void AddValidatorsFromAssemblyContaining(Type type, bool throwForNonPublicValidators = true)
+        public void AddValidatorsFromAssemblyContaining(Type type, bool throwForNonPublicValidators = true, bool throwForNoValidatorsFound = true)
         {
-            AddValidatorsFromAssembly(type.GetTypeInfo().Assembly, throwForNonPublicValidators);
+            AddValidatorsFromAssembly(type.GetTypeInfo().Assembly, throwForNonPublicValidators, throwForNoValidatorsFound);
         }
 
-        public void AddValidatorsFromAssembly(Assembly assembly, bool throwForNonPublicValidators = true)
+        public void AddValidatorsFromAssembly(Assembly assembly, bool throwForNonPublicValidators = true, bool throwForNoValidatorsFound = true)
         {
+            var assemblyName = assembly.GetName().Name;
             if (throwForNonPublicValidators)
             {
                 var openGenericType = typeof(IValidator<>);
@@ -70,13 +71,19 @@ namespace NServiceBus
                     .ToList();
                 if (nonPublicValidators.Any())
                 {
-                    throw new Exception($"Found some non public validators were found:{Environment.NewLine}{string.Join(Environment.NewLine, nonPublicValidators.Select(x => x.FullName))}");
+                    throw new Exception($"Found some non public validators were found in {assemblyName}:{Environment.NewLine}{string.Join(Environment.NewLine, nonPublicValidators.Select(x => x.FullName))}.");
                 }
+            }
+
+            var results = AssemblyScanner.FindValidatorsInAssembly(assembly).ToList();
+            if (throwForNoValidatorsFound && !results.Any())
+            {
+                throw new Exception($"No validators were found in {assemblyName}.");
             }
 
             endpoint.RegisterComponents(components =>
             {
-                foreach (var result in AssemblyScanner.FindValidatorsInAssembly(assembly))
+                foreach (var result in results)
                 {
                     components.ConfigureComponent(result.ValidatorType, dependencyLifecycle);
                 }
