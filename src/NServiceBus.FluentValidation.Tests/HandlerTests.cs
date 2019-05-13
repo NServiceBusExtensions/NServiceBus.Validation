5b8eba1393068db4eb58f02acf1c6e66c8967fbd
Simon Cropp <simon.cropp@gmail.com>
2019-05-13 14:47:24 +1000

    Update HandlerTests.cs

--- src/NServiceBus.FluentValidation.Tests/HandlerTests.cs
+++ src/NServiceBus.FluentValidation.Tests/HandlerTests.cs
@@ -7,31 +7,30 @@ public class HandlerTests :
     TestBase
 {
     [Fact]
-    public async Task Validate_TestableMessageHandlerContext()
+    public Task Validate_TestableMessageHandlerContext()
     {
         var handlerContext = new TestableMessageHandlerContext();
 
         var handler = new MyHandler();
         var message = new MyMessage();
-        await handler.Handle(message, handlerContext);
-        await Assert.ThrowsAsync<MessageValidationException>(() => handlerContext.Validate(message));
+        return Assert.ThrowsAsync<MessageValidationException>(() => handlerContext.Validate(message));
     }
 
     [Fact]
-    public async Task Validate_ValidatingHandlerContext()
+    public Task Validate_ValidatingHandlerContext()
     {
         var message = new MyMessage();
         var handlerContext = ValidatingContext.Build(message);
         var handler = new MyHandler();
-        await Assert.ThrowsAsync<MessageValidationException>(() => handlerContext.Run(handler));
+        return Assert.ThrowsAsync<MessageValidationException>(() => handlerContext.Run(handler));
     }
 
     [Fact]
-    public async Task Validate_ValidatingHandlerContext_Static_Run()
+    public Task Validate_ValidatingHandlerContext_Static_Run()
     {
         var message = new MyMessage();
         var handler = new MyHandler();
-        await Assert.ThrowsAsync<MessageValidationException>(() => ValidatingContext.Run(handler, message));
+        return Assert.ThrowsAsync<MessageValidationException>(() => ValidatingContext.Run(handler, message));
     }
 }
 
