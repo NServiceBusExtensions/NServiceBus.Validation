4f9c08bb702e54a183384749a61e6b64487f6f59
Simon Cropp <simon.cropp@gmail.com>
2019-06-27 12:27:14 +1000

    split file

--- src/NServiceBus.FluentValidation.Testing/ValidatingContext.cs
+++ src/NServiceBus.FluentValidation.Testing/ValidatingContext.cs
@@ -4,33 +4,6 @@ using System.Threading.Tasks;
 
 namespace NServiceBus.Testing
 {
-    public static class ValidatingContext
-    {
-        public static ValidatingContext<TMessage> Build<TMessage>(TMessage message)
-        {
-            Guard.AgainstNull(message, nameof(message));
-            return new ValidatingContext<TMessage>(message);
-        }
-
-        public static async Task<ValidatingContext<TMessage>> Run<TMessage>(IHandleMessages<TMessage> handler, TMessage message)
-        {
-            Guard.AgainstNull(message, nameof(message));
-            Guard.AgainstNull(handler, nameof(handler));
-            var context = Build(message);
-            await context.Run(handler);
-            return context;
-        }
-
-        public static async Task<ValidatingContext<TMessage>> Run<TMessage>(IHandleTimeouts<TMessage> handler, TMessage message)
-        {
-            Guard.AgainstNull(message, nameof(message));
-            Guard.AgainstNull(handler, nameof(handler));
-            var context = Build(message);
-            await context.Run(handler);
-            return context;
-        }
-    }
-
     public class ValidatingContext<TMessage> :
         TestableMessageHandlerContext
     {
