dfc6ef9fe3bf5de50df8478dcedddd5249d4168c
Simon Cropp <simon.cropp@gmail.com>
2019-10-08 20:13:14 +1100

    Update IncomingTests.cs

--- src/NServiceBus.FluentValidation.Tests/IncomingTests.cs
+++ src/NServiceBus.FluentValidation.Tests/IncomingTests.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
-using ApprovalTests;
 using NServiceBus;
 using NServiceBus.Features;
 using NServiceBus.FluentValidation;
