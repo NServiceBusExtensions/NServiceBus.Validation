c692922faed96579ccaf28dd5e92cae505886567
Simon Cropp <simon.cropp@gmail.com>
2024-07-26 08:15:54 +1000

    ValidatingSession

---
+++ src/NServiceBus.FluentValidation.Testing/ValidatingSession.cs
@@ -0,0 +1,21 @@
+﻿namespace NServiceBus.FluentValidation.Testing;
+
+public class ValidatingSession(
+    IServiceProvider? provider = null)
+    :
+        RecordingMessageSession
+{
+    protected IServiceProvider? Provider { get; } = provider;
+
+    public override async Task Send(object message, SendOptions options, Cancel cancel = default)
+    {
+        await TestContextValidator.ValidateWithTypeRedirect(message, options, Provider);
+        await base.Send(message, options, cancel);
+    }
+
+    public override async Task Publish(object message, PublishOptions options, Cancel cancel = default)
+    {
+        await TestContextValidator.ValidateWithTypeRedirect(message, options, Provider);
+        await base.Publish(message, options, cancel);
+    }
+}
