aaf6d117be557be36c1e637850071e1d6691f500
Simon Cropp <simon.cropp@gmail.com>
2024-07-29 14:57:52 +1000

    refs

--- src/NServiceBus.FluentValidation.Testing/GlobalUsings.cs
+++ src/NServiceBus.FluentValidation.Testing/GlobalUsings.cs
@@ -1,3 +1,4 @@
-﻿global using System.Globalization;
+﻿global using System.ComponentModel;
+global using System.Globalization;
 global using FluentValidation;
 global using VerifyTests.NServiceBus;
--- src/NServiceBus.FluentValidation.Testing/ValidatingContext.cs
+++ src/NServiceBus.FluentValidation.Testing/ValidatingContext.cs
@@ -25,4 +25,18 @@ public partial class ValidatingContext(
         await TestContextValidator.ValidateWithTypeRedirect(message, options, Provider);
         await base.Publish(message, options);
     }
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public override int GetHashCode() =>
+        // ReSharper disable once BaseObjectGetHashCodeCallInGetHashCode
+        base.GetHashCode();
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public override string? ToString() =>
+        base.ToString();
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public override bool Equals(object? obj) =>
+        // ReSharper disable once BaseObjectEqualsIsObjectEquals
+        base.Equals(obj);
 }
